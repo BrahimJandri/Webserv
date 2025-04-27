1887f080e219ce863a53cdac940a9651707bae3a
BrahimJandri <bzaza066@gmail.com>
2025-04-27 10:23:31 +0100

    create a request class

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int ac, char **av)
     if (ac == 2)
     {
         ConfigParser parser(av[1]);
-        requestParser req;
+        requestParser req; // hada class fih parsed request
 
         if (!parser.parse())
             return 1;
