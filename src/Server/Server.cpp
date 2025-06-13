92caa159fbe030e138013c070aaa907729d1d623
BrahimJandri <bzaza066@gmail.com>
2025-06-13 11:04:24 +0100

    add image to webserv

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -24,7 +24,7 @@ int create_server_socket(const std::string &host, int port)
 	struct sockaddr_in addr;
 	std::memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);                    // This function is used to convert the unsigned int from machine byte order to network byte order.
+	addr.sin_port = htons(port);					// This function is used to convert the unsigned int from machine byte order to network byte order.
 	addr.sin_addr.s_addr = inet_addr(host.c_str()); // It is used when we don't want to bind our socket to any particular IP and instead make it listen to all the available IPs.
 
 	if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
@@ -83,15 +83,50 @@ void handle_requests(int server_fd, requestParser &req)
 			std::cout << it->first << ": " << it->second << std::endl;
 		}
 
-		std::string body = "<h1>Hello from Webserv</h1>";
-		std::string response =
-			"HTTP/ 200 OK\r\n"
-			"Content-Type: text/html\r\n"
-			"Content-Length: " +
-			to_string_c98(body.length()) + "\r\n"
-											"\r\n" +
-			body;
-		write(client_fd, response.c_str(), response.length());
-		close(client_fd);
+		// std::string body = "<h1>Hello from Webserv</h1>";
+		// std::string response =
+		// 	"HTTP/ 200 OK\r\n"
+		// 	"Content-Type: image/jpg\r\n";
+		// write(client_fd, response.c_str(), response.length());
+		// close(client_fd);
+		{
+			const char *image_path = "./images/agadir.jpg";
+
+			// Open image file in binary mode
+			int img_fd = open(image_path, O_RDONLY);
+			if (img_fd < 0)
+			{
+				std::string error_response = "HTTP/1.1 404 Not Found\r\n\r\nImage not found";
+				write(client_fd, error_response.c_str(), error_response.length());
+				close(client_fd);
+				return;
+			}
+
+			// Get image size
+			struct stat st;
+			fstat(img_fd, &st);
+			size_t img_size = st.st_size;
+
+			// Construct HTTP response headers
+			std::ostringstream oss;
+			oss << "HTTP/1.1 200 OK\r\n"
+				<< "Content-Type: image/jpeg\r\n"
+				<< "Content-Length: " << img_size << "\r\n"
+				<< "Connection: close\r\n\r\n"; // End of headers
+
+			std::string header = oss.str();
+			write(client_fd, header.c_str(), header.length());
+
+			// Send image data
+			char buffer[4096];
+			ssize_t bytes_read;
+			while ((bytes_read = read(img_fd, buffer, sizeof(buffer))) > 0)
+			{
+				write(client_fd, buffer, bytes_read);
+			}
+
+			close(img_fd);
+			close(client_fd);
+		}
 	}
 }
--- src/Server/Server.hpp
+++ src/Server/Server.hpp
@@ -2,6 +2,13 @@
 
 #include <string>
 #include "../HTTP/Request.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 int create_server_socket(const std::string &host, int port);
 void handle_requests(int server_fd, requestParser &req);
